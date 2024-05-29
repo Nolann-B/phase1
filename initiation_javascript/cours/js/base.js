24e417ab7cbaa26df45478fa12cda39f511c3f35
Machamp <nolann.bourimech@lepoles.org>
2024-05-29 16:34:14 +0200

    29_05
    js_base

---
+++ initiation_javascript/cours/js/base.js
@@ -0,0 +1,21 @@
+// syntaxe de base 
+
+// -- 1 : Declarer une variable 
+var prenom,
+    monNom = "izadi";
+var prenomPourMonProjet;
+// -- 2 : Afffecter une valeur à une variable 
+prenom= "Mila";
+
+// alert (prenom);
+// alert (monNom);
+
+// -- 3 : Une instruction se temrine TOUIJOURS par un point-virgule, aussi il est possible d'écrire plusioeursd instructions sur une seule lige 
+//  inst_1;
+// inst_2; inst_3
+
+//  -- 5 : Afficher dans la conssole (ici, la valeur de ma variable Prenom)
+console.log(prenom);
+
+document.write(prenom + "<br>")
+document.write("A la pause on se la met")
