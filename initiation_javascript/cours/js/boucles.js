af80059c4513a85fc832069a9f989bdc170a73c0
Machamp <nolann.bourimech@lepoles.org>
2024-06-06 16:53:34 +0200

    06_06
    js_boucle et tp_ce soir

---
+++ initiation_javascript/cours/js/boucles.js
@@ -0,0 +1,15 @@
+var age= parseInt(window.prompt ("age svp"));
+var majeur = 18;
+if (age>=majeur) {
+    document.write("Bravo")
+}else{
+    document.write("ba non")};
+
+while (age <= 3 || age >= 120){
+    document.write("bravo")
+}
+
+
+
+
+
